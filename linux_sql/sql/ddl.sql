966e4ab981d1524b159563b63e7467b210dcf6ce
Nasr Syed <nasrsyed20@gmail.com>
2021-11-02 16:03:39 +0000

    adding ddl sql file to feature branch

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,26 @@
+\c host_agent;
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info
+  (
+     id               SERIAL NOT NULL PRIMARY KEY,
+     hostname         VARCHAR NOT NULL UNIQUE,
+     cpu_number       INT NOT NULL,
+     cpu_architecture VARCHAR NOT NULL,
+	 cpu_model        VARCHAR NOT NULL,
+     cpu_mhz          FLOAT(3),
+	 L2_cache         INT NOT NULL,
+	 total_mem        INT NOT NULL,
+	 timestamp        TIMESTAMP NOT NULL
+  );
+
+
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
+(
+    "timestamp"    TIMESTAMP NOT NULL,
+    host_id        SERIAL REFERENCES host_info(id) NOT NULL,
+    memory_free    INT NOT NULL,
+    cpu_idle       INT NOT NULL,
+    cpu_kernel     INT NOT NULL,
+    disk_io        INT NOT NULL,
+    disk_available INT NOT NULL
+);
