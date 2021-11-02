34536a4abc216a1f4239bdf73c8d354f54fa15e2
Nasr Syed <nasrsyed20@gmail.com>
2021-11-02 16:06:05 +0000

    fixed indentation issues

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -6,11 +6,11 @@ CREATE TABLE IF NOT EXISTS PUBLIC.host_info
      hostname         VARCHAR NOT NULL UNIQUE,
      cpu_number       INT NOT NULL,
      cpu_architecture VARCHAR NOT NULL,
-	 cpu_model        VARCHAR NOT NULL,
+     cpu_model        VARCHAR NOT NULL,
      cpu_mhz          FLOAT(3),
-	 L2_cache         INT NOT NULL,
-	 total_mem        INT NOT NULL,
-	 timestamp        TIMESTAMP NOT NULL
+     L2_cache         INT NOT NULL,
+     total_mem        INT NOT NULL,
+     timestamp        TIMESTAMP NOT NULL
   );
 
 
